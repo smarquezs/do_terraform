f9da94f9157f389733b6a49bbb3413e4af516d1a
Sergio Márquez <smarquezs@gmail.com>
2021-04-15 21:12:00 -0400

    Add new node pool for DBs.

--- kubernetes/main.tf
+++ kubernetes/main.tf
@@ -2,12 +2,31 @@ resource "digitalocean_kubernetes_cluster" "test" {
   name    = "test"
   region  = "nyc1"
   version = "1.20.2-do.0"
-
   node_pool {
-    name       = "worker-nodes"
-    size       = "s-1vcpu-2gb"
+    name       = "worker-nodes-4g"
+    size       = "s-2vcpu-4gb"
     auto_scale = true
     min_nodes  = 1
-    max_nodes  = 10
+    max_nodes  = 8
+  }
+}
+
+resource "digitalocean_kubernetes_node_pool" "databases" {
+  cluster_id = digitalocean_kubernetes_cluster.test.id
+
+  name       = "databases-pool"
+  size       = "s-4vcpu-8gb"
+  node_count = 1
+  tags       = ["backend"]
+
+  labels = {
+    service  = "db"
+    priority = "true"
+  }
+
+  taint {
+    key    = "db"
+    value  = "true"
+    effect = "NoSchedule"
   }
 }
